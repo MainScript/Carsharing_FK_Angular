7267c3d7bf969f3e899b877f6241991bebb227b4
MainScript <mcmainscript@gmail.com>
2022-07-15 18:22:04 +0200

    add test

--- src/app/pipes/car.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CarPipe } from './car.pipe';
-
-describe('CarPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CarPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
@@ -6,11 +7,37 @@ describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(AuthService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should detect invalid username', () => {
+    const invalid = [0, 'Username must contain only letters and numbers'];
+    const valid = [-1, ''];
+    
+    let result = service.register('a1b2c3d4e5', '', '');
+    expect(result).toEqual(valid);
+
+    result = service.register('', '', '');
+    expect(result).toEqual(invalid);
+
+
+    result = service.register('a1b2c3d4e5!', '', '');
+    expect(result).toEqual(invalid);
+
+    result = service.register('#asbvsajhgdhj', '', '');
+    expect(result).toEqual(invalid);
+
+    result = service.register('DROP * FROM cars;', '', '');
+    expect(result).toEqual(invalid);
+
+    result = service.register('a b', '', '');
+    expect(result).toEqual(invalid);
   });
 });
